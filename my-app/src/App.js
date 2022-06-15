02b9dfca6883c252801e8b26338fd562e3134473
Raj Kumar Lama <rajlama901@gmail.com>
2022-06-15 16:58:14 +0200

    css file added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,16 +11,18 @@ import Button from "./components/Button"
 import Home from "./pages/Home"
 import Chuck from "./pages/Chuck"
 
+import "./styles/main.css"
+
 const App = () => {
     return <>
         {/* <h1>App</h1> */}
         {/* <Greeting firstname = "RK" lastname = "Lama" />
         <Button>Add</Button> */}
         <BrowserRouter>
-            <div>
-                <Link to="/">Home</Link><br/>
-                <Link to="/button">Button</Link><br/>
-                <Link to="/chuck">Chuck</Link>
+            <div id="nav">
+                <Link to="/">Home</Link>
+                <Link to="/button">Button</Link>
+                <Link to="/chuck">Chuck Norris Fact</Link>
             </div>
             <Routes>
                 <Route path="/" element={<Home />} />
